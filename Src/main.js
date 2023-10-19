dec6d11c3fd7195f5efc4b065444ce61b995d779
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-19 12:33:46 +0900

    Make Empty

--- Src/main.js
+++ Src/main.js
@@ -1,11 +1,17 @@
 async function main() {
     await Sleep(1000);
+
     DrawText("Hello World!", 100, 100);
+
     await Sleep(1000);
+    
+    SetColor("red");
+    SetFont("50px serif");
     DrawText("Let's Coding!", 100, 200);
 
     await Sleep(1000);
 
     const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0];
     DrawImage(chick, 100, 300, 100, 100);
+
 }
