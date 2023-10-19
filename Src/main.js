8088ee014c30f8fe4c3681f72de067faa14a5713
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-19 11:21:32 +0900

    Picture loading

--- Src/main.js
+++ Src/main.js
@@ -1,3 +1,11 @@
 async function main() {
+    await Sleep(1000);
     DrawText("Hello World!", 100, 100);
+    await Sleep(1000);
+    DrawText("Let's Coding!", 100, 200);
+
+    await Sleep(1000);
+
+    const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0];
+    DrawImage(chick, 100, 300, 100, 100);
 }
