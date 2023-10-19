a953578c746f664ae239e13c1a09a442febe2346
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-19 12:36:29 +0900

    Make main code better

--- Src/main.js
+++ Src/main.js
@@ -11,7 +11,18 @@ async function main() {
 
     await Sleep(1000);
 
+    
     const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0];
-    DrawImage(chick, 100, 300, 100, 100);
+    
+    while (true){
+        //clear
+        SetColor("white");
+        DrawRect(0, 0, GetCanvasSize()[0], GetCanvasSize()[1]);
+
+        DrawImage(chick, GetMouseX(), GetMouseY(), 100, 100);
+    
+        //cool time
+        await Sleep(1000/16);
+    }
 
 }
