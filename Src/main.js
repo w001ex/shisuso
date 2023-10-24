6d85c05a8d5465c30311cc5ca5dac0e8a774defa
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-24 15:13:12 +0900

    remove semicolon

--- Src/main.js
+++ Src/main.js
@@ -1,28 +1,27 @@
 async function main() {
-    await Sleep(1000);
+    await Sleep(1000)
 
-    DrawText("Hello World!", 100, 100);
+    DrawText("Hello World!", 100, 100)
 
-    await Sleep(1000);
+    await Sleep(1000)
     
-    SetColor("red");
-    SetFont("50px serif");
-    DrawText("Let's Coding!", 100, 200);
+    SetColor("red")
+    SetFont("50px serif")
+    DrawText("Let's Coding!", 100, 200)
 
-    await Sleep(1000);
+    await Sleep(1000)
 
     
-    const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0];
+    const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0]
     
     while (true){
         //clear
-        SetColor("white");
-        DrawRect(0, 0, GetCanvasSize()[0], GetCanvasSize()[1]);
+        SetColor("white")
+        DrawRect(0, 0, GetCanvasSize()[0], GetCanvasSize()[1])
 
-        DrawImage(chick, GetMouseX(), GetMouseY(), 100, 100);
+        DrawImage(chick, GetMouseX(), GetMouseY(), 100, 100)
     
         //cool time
-        await Sleep(1000/16);
+        await Sleep(1000/16)
     }
-
 }
