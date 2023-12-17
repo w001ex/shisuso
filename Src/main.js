f716c1357f27fafd508a620847170fd89c5493f8
konbraphat51 <konbraphat@gmail.com>
2023-12-17 15:16:29 +0900

    DrawText

--- Src/main.js
+++ Src/main.js
@@ -1,27 +1,7 @@
 async function main() {
-    await Sleep(1000)
+	//""つける
+	DrawText("ああ", 300, 500)
 
-    DrawText("Hello World!", 100, 100)
-
-    await Sleep(1000)
-    
-    SetColor("red")
-    SetFont("50px serif")
-    DrawText("Let's Coding!", 100, 200)
-
-    await Sleep(1000)
-
-    
-    const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0]
-    
-    while (true){
-        //clear
-        SetColor("white")
-        DrawRect(0, 0, GetCanvasSize()[0], GetCanvasSize()[1])
-
-        DrawImage(chick, GetMouseX(), GetMouseY(), 100, 100)
-    
-        //cool time
-        await Sleep(1000/16)
-    }
+	//数字はつけなくても大丈夫
+	DrawText(1247809734, 300, 550)
 }
