2c6b4c5caa18de667959ac3bb38b54315b715538
konbraphat51 <konbraphat@gmail.com>
2023-12-17 15:30:55 +0900

    変数

--- Src/main.js
+++ Src/main.js
@@ -12,9 +12,20 @@ async function main() {
 	// DrawText("ああ", 300, 200)
 
 	//計算
-	DrawText(1 + 1, 300, 300)
-	DrawText(1 - 1, 300, 350)
-	DrawText(1 * 1, 300, 400)
-	DrawText(1 / 2, 300, 450)
-	DrawText(3 % 2, 300, 500)
+	// DrawText(1 + 1, 300, 300)
+	// DrawText(1 - 1, 300, 350)
+	// DrawText(1 * 1, 300, 400)
+	// DrawText(1 / 2, 300, 450)
+	// DrawText(3 % 2, 300, 500)
+
+	//変数
+	SetFont("100px arial")
+	let a = 1 //変数を作るときは"let"
+	DrawText(a, 300, 300)
+
+	a = 2
+	DrawText(a, 300, 400)
+
+	a += 1 //=の左に演算子で変数の中身を計算
+	DrawText(a, 300, 500)
 }
