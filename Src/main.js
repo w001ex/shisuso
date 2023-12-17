9405e12f6025109b407a7f95f3af3ed9467bd8cd
konbraphat51 <konbraphat@gmail.com>
2023-12-17 15:25:34 +0900

    計算

--- Src/main.js
+++ Src/main.js
@@ -1,13 +1,20 @@
 async function main() {
-	//""つける
-	DrawText("ああ", 300, 500)
+	// //""つける
+	// DrawText("ああ", 300, 500)
 
-	//数字はつけなくても大丈夫
-	DrawText(1247809734, 300, 550)
+	// //数字はつけなくても大丈夫
+	// DrawText(1247809734, 300, 550)
 
-	//色はHEX(16進数）を#から始める
-	//https://htmlcolorcodes.com/
-	DrawText("いい", 300, 100)
-	SetColor("#4EB72E") //緑
-	DrawText("ああ", 300, 200)
+	// //色はHEX(16進数）を#から始める
+	// //https://htmlcolorcodes.com/
+	// DrawText("いい", 300, 100)
+	// SetColor("#4EB72E") //緑
+	// DrawText("ああ", 300, 200)
+
+	//計算
+	DrawText(1 + 1, 300, 300)
+	DrawText(1 - 1, 300, 350)
+	DrawText(1 * 1, 300, 400)
+	DrawText(1 / 2, 300, 450)
+	DrawText(3 % 2, 300, 500)
 }
