e905b7f44416a81faff332cc083f054e7a7ebc65
konbraphat51 <konbraphat@gmail.com>
2023-12-17 15:22:31 +0900

    SetColor

--- Src/main.js
+++ Src/main.js
@@ -4,4 +4,10 @@ async function main() {
 
 	//数字はつけなくても大丈夫
 	DrawText(1247809734, 300, 550)
+
+	//色はHEX(16進数）を#から始める
+	//https://htmlcolorcodes.com/
+	DrawText("いい", 300, 100)
+	SetColor("#4EB72E") //緑
+	DrawText("ああ", 300, 200)
 }
